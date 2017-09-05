6de643a7aca2b702ed5bdee67e605c6620640b67
janita <zcj880902>
2017-09-05 14:45:06 +0800

    登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main';
-
 import Login from '@/components/Login';
-
 import Form from '@/components/nav1/Form';
-import Table from '@/components/nav1/Table';
-import user from '@/components/nav1/user';
 import Home from '@/components/Home';
-
 import stoneList from '@/components/stone/stoneList';
-
-
 
 Vue.use(Router);
 
@@ -31,9 +24,7 @@ export default new Router({
       iconCls: 'el-icon-message',//图标样式class
       children: [
         { path: '/main', component: Main, name: '主页', hidden: true },
-        { path: '/table', component: Table, name: 'Table' },
         { path: '/form', component: Form, name: 'Form' },
-        { path: '/user', component: user, name: '列表' },
       ]
     },
 
@@ -44,45 +35,6 @@ export default new Router({
       iconCls: 'el-icon-message',//图标样式class
       children: [
         { path: '/stoneList', component: stoneList, name: '石头列表' },
-      ]
-    },
-
-    {
-      path: '/',
-      component: Home,
-      name: '产品管理',
-      iconCls: 'el-icon-message',//图标样式class
-      children: [
-        { path: '/main', component: Main, name: '主页', hidden: true },
-        { path: '/table', component: Table, name: 'Table' },
-        { path: '/form', component: Form, name: 'Form' },
-        { path: '/user', component: user, name: '列表' },
-      ]
-    },
-
-    {
-      path: '/',
-      component: Home,
-      name: '产品管理',
-      iconCls: 'el-icon-message',//图标样式class
-      children: [
-        { path: '/main', component: Main, name: '主页', hidden: true },
-        { path: '/table', component: Table, name: 'Table' },
-        { path: '/form', component: Form, name: 'Form' },
-        { path: '/user', component: user, name: '列表' },
-      ]
-    },
-
-    {
-      path: '/',
-      component: Home,
-      name: '产品管理',
-      iconCls: 'el-icon-message',//图标样式class
-      children: [
-        { path: '/main', component: Main, name: '主页', hidden: true },
-        { path: '/table', component: Table, name: 'Table' },
-        { path: '/form', component: Form, name: 'Form' },
-        { path: '/user', component: user, name: '列表' },
       ]
     },
   ]

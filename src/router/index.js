2809cfe37b75969377789cdffde0b26127a981d4
janita <zcj880902>
2017-09-05 12:46:18 +0800

    组件使用

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import Main from '@/components/Main';
 
 import Login from '@/components/Login';
 
+import Form from '@/components/nav1/Form';
+import Table from '@/components/nav1/Table';
+import user from '@/components/nav1/user';
+import Home from '@/components/Home';
+
+
+
 Vue.use(Router);
 
 export default new Router({
@@ -14,10 +21,18 @@ export default new Router({
       name: '',
       hidden: true
     },
+
     {
       path: '/',
-      name: 'Main',
-      component: Main
-    }
+      component: Home,
+      name: '导航一',
+      iconCls: 'el-icon-message',//图标样式class
+      children: [
+        { path: '/main', component: Main, name: '主页', hidden: true },
+        { path: '/table', component: Table, name: 'Table' },
+        { path: '/form', component: Form, name: 'Form' },
+        { path: '/user', component: user, name: '列表' },
+      ]
+    },
   ]
 })

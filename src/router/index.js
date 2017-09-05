190df8edf16a9f71ca88303154f0deabddb07da9
janita <zcj880902>
2017-09-05 11:48:13 +0800

    js-cookie

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello';
 
+import Login from '@/components/Login';
+
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      component: Login,
+      name: '',
+      hidden: true
+    },
     {
       path: '/',
       name: 'Hello',
